4327e04f0bf952e3e07a7a4886e2369d271f8cea
unknown <dgs@umich.edu>
2013-10-03 18:51:43 -0400

    before deleting old sln

---
+++ DaveBlog.Web.Tests.Unit/Class1.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaveBlog.Web.Tests.Unit
+{
+    public class HomeControllerTests
+    {
+        [Test]
+        public void ActionIndexShouldDisplayPosts()
+        {
+            // arrange
+            var controller = new HomeController();
+            // act
+            // assert
+        }
+    }
+}
