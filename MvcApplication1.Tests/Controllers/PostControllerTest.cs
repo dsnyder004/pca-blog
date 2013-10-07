db143656987df41e7e454afa52b2eabd43f1c927
unknown <dgs@umich.edu>
2013-10-07 11:15:44 -0400

    Add some new tests

---
+++ MvcApplication1.Tests/Controllers/PostControllerTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcApplication1.Controllers;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Tests.Controller
+{
+    [TestClass]
+    public class PostControllerTest
+    {
+        [TestMethod]
+        public void EditPost()
+        {
+            // Arrange
+            PostController controller = new PostController();
+
+            // Act
+            int id = controller.createPost();
+            ViewResult result = controller.Edit(id) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void CreatePost()
+        {
+            // Arrange
+            PostController controller = new PostController();
+
+            // Act
+            ViewResult result = controller.Create() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void DeletePost()
+        {
+            // Arrange
+            PostController controller = new PostController();
+
+            // Act
+            int id = controller.createPost();
+            ViewResult result = controller.Delete(id) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
